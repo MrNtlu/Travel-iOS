ebea935292d85fdbe9dc42836f6a1bad4539ea0c
MrNtlu <97burakfidan97@gmail.com>
2022-08-08 17:54:33 +0300

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Burak Fidan on 8.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
