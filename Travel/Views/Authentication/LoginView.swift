676ffa3b53abde653d4956597ae0fa7c7610d567
MrNtlu <97burakfidan97@gmail.com>
2022-08-09 19:04:14 +0300

    Welcome view and login view implemented.

--- Travel/Views/Authentication/LoginView.swift
+++ Travel/Views/Authentication/LoginView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var isSigningIn = false
+    
+    @State private var emailTextField = ""
+    @State private var passwordTextField = ""
+    @State private var showPassword = false
+    
     var body: some View {
         ZStack {
             GeometryReader { geometry in
@@ -18,36 +25,99 @@ struct LoginView: View {
                     .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
                     .blur(radius: 2.2)
             }
-            VStack(spacing: 6) {
-                Text("Collect Moments Not Things")
-                    .font(Font.custom("Pacifico-Regular", size: 46))
+            if !isSigningIn {
+                VStack(spacing: 6) {
+                    Text("Collect Moments Not Things")
+                        .font(Font.custom("Pacifico-Regular", size: 46))
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .frame(maxHeight: .infinity)
+                    
+                    SignInButton {
+                        self.isSigningIn = true
+                    }
+                    
+                    Button("Don't have any account? Register") {
+                        print("Clicked")
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .font(.system(size: 18, weight: .semibold))
                     .foregroundColor(.black)
-                    .multilineTextAlignment(.center)
-                    .frame(maxHeight: .infinity)
-                
-                Button("Sign in") {
-                    print("Clicked")
+                    .cornerRadius(12)
                 }
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .padding(.all, 28)
+            } else {
+                VStack(spacing: 8) {
+                    HStack {
+                        Image(systemName: "envelope.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20,height: 20)
+                            .foregroundColor(.black)
+                        
+                        TextField("Email", text: $emailTextField)
+                            .keyboardType(.emailAddress)
+                            .submitLabel(.next)
+                            .font(.body)
+                    }
+                    .padding()
+                    .background(Rectangle().fill(.white).cornerRadius(12))
+                    
+                    HStack {
+                        Image(systemName: "lock.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20,height: 20)
+                            .foregroundColor(.black)
+                        
+                        if showPassword {
+                            TextField("Password", text: $passwordTextField)
+                                .submitLabel(.done)
+                                .font(.body)
+                        } else {
+                            SecureField("Password", text: $passwordTextField)
+                                .submitLabel(.done)
+                                .font(.body)
+                        }
+                        
+                        Button(action: { self.showPassword.toggle()}) {
+                            Image(systemName: self.showPassword ? "eye.slash.fill": "eye.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24,height: 24)
+                                .foregroundColor(.secondary)
+                            
+                        }
+                    }
+                    .padding()
+                    .background(Rectangle().fill(.white).cornerRadius(12))
+                    
+                    SignInButton {
+                        print("Signing in")
+                    }
+                }
+                .padding(.all, 28)
+            }
+        }
+    }
+}
+
+struct SignInButton: View {
+    
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text("Sign In")
                 .frame(minWidth: 0, maxWidth: .infinity)
-                .padding()
-                .background(Color.blue)
+                .padding(.vertical, 12)
+                .background(Color("Teal"))
                 .foregroundColor(.white)
                 .font(.system(size: 22, weight: .semibold))
                 .cornerRadius(16)
-                
-                Button("Don't have any account? Register") {
-                    print("Clicked")
-                }
-                .padding()
-                .frame(maxWidth: .infinity)
-                .font(.system(size: 18, weight: .semibold))
-                .foregroundColor(.black)
-                .cornerRadius(12)
-            }
-            .frame(maxHeight: .infinity, alignment: .bottom)
-            .padding(.bottom, 32)
-            .padding(.horizontal, 16)
-        }
+        }.padding(.top, 18)
     }
 }
 
